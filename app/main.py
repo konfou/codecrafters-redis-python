af1711546edacf15e52a269cdd43ac60940a9b59
Konstantinos Foutzopoulos <mail@konfou.xyz>
2023-02-03 20:24:18 +0200

    stage 6

--- app/main.py
+++ app/main.py
@@ -49,13 +49,45 @@ class RESP:
 
 
 class Store:
-    def __init__(self):
-        pass
+    lock = threading.Lock()
 
-    def err_args_num(self, cmd):
+    def __init__(self):
+        self.store = {}
+
+    # XXX: maybe move those to RESP class
+    def resp_err_arity(self, cmd):
         return b"-ERR wrong number of arguments for '%b' command\r\n" % cmd
 
+    def resp_bulk_string(self, string):
+        return b'$%d\r\n%b\r\n' % (len(string), string)
+
+    def store_set(self, args):
+        cmd = b'set'
+
+        if args is None or len(args) != 2:
+            ret = self.resp_err_arity(cmd)
+        else:
+            self.store[args[0]] = args[1]
+            ret = b'+OK\r\n'
+
+        return ret
+
+    def store_get(self, args):
+        cmd = b'get'
+
+        if args is None or len(args) != 1:
+            ret = self.resp_err_arity(cmd)
+        elif args[0] in self.store:
+            value = self.store[args[0]]
+            ret = self.resp_bulk_string(value)
+        else:
+            ret = b'$-1\r\n'
+
+        return ret
+
     def run(self, cmd, args=None):
+        # XXX: maybe let default args be [] so don't have to check both
+        # whether is None and args len
         if isinstance(cmd, list):
             cmd, *args = cmd
 
@@ -63,14 +95,19 @@ class Store:
             if args is None or len(args) == 0:
                 ret = b'+PONG\r\n'
             elif len(args) == 1:
-                ret = b'$%d\r\n%b\r\n' % (len(args[0]), args[0])
+                ret = self.resp_bulk_string(args[0])
             else:
-                ret = self.err_args_num(cmd)
+                ret = self.resp_err_arity(cmd)
         elif cmd.upper() == b'ECHO':
             if args is None or len(args) != 1:
-                ret = self.err_args_num(cmd)
+                ret = self.resp_err_arity(cmd)
             else:
-                ret = b'$%d\r\n%b\r\n' % (len(args[0]), args[0])
+                ret = self.resp_bulk_string(args[0])
+        elif cmd.upper() == b'SET':
+            with Store.lock:
+                ret = self.store_set(args)
+        elif cmd.upper() == b'GET':
+            ret = self.store_get(args)
         else:
             ret = b"-ERR unknown command '%s'" % cmd
 
